d273c26e5e0ae3c6585d06e5439510a45f0a24e0
qyuz <qyuz555@gmail.com>
2024-12-29 23:48:20 +0200

    create database page with tests

--- load/notion_page.go
+++ load/notion_page.go
@@ -11,13 +11,36 @@ var (
 	client = notionapi.NewClient("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")
 )
 
-func getPageById(id string) *notionapi.Page {
+func GetPageById(id string) *notionapi.Page {
 	pageId := notionapi.PageID(id)
 	page, err := client.Page.Get(context.Background(), pageId)
 	if err != nil {
 		if strings.Contains(err.Error(), "Could not find page with ID") {
 			return nil
 		}
+		panic(err)
+	}
+	return page
+}
+
+func CreateDatabasePage() *notionapi.Page {
+	page, err := client.Page.Create(context.Background(), &notionapi.PageCreateRequest{
+		Parent: notionapi.Parent{
+			DatabaseID: "16626ea29ff180e3a9d1e83f92622638",
+		},
+		Properties: notionapi.Properties{
+			"Name": notionapi.TitleProperty{
+				Title: []notionapi.RichText{
+					{
+						Text: &notionapi.Text{
+							Content: "New Page",
+						},
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
 		panic(err)
 	}
 	return page
